df1952117838c9e742c0a57beb315ee5bc659016
ik012982i9 <a@>
2024-03-14 13:21:44 +0100

    implementar los radiobuttons en la parte ESTE

--- src/HITO1.java
+++ src/HITO1.java
@@ -17,6 +17,27 @@ public class HITO1 extends JFrame {
         getContentPane().add(checkboxPanel,BorderLayout.NORTH);
 
 
+
+        JPanel radioPanel = new JPanel();
+        radioPanel.setPreferredSize(new Dimension(250, getHeight()));
+        radioPanel.setBackground(Color.LIGHT_GRAY);
+        JRadioButton rb1 = new JRadioButton("OPT 1");
+        JRadioButton rb2 = new JRadioButton("OPT 2");
+        JRadioButton rb3 = new JRadioButton("OPT 3");
+        ButtonGroup group = new ButtonGroup();
+        group.add(rb1);
+        group.add(rb2);
+        group.add(rb3);
+        rb1.setSelected(true);
+        JRadioButton[] radioButtons = {rb1, rb2, rb3};
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new BoxLayout(panel2, BoxLayout.Y_AXIS));
+        for (JRadioButton radioButton : radioButtons) {
+            panel2.add(radioButton);
+        }
+        getContentPane().add(panel2, BorderLayout.EAST);
+
+
         pack();
         setVisible(true);
     }
