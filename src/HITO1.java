7d09fea0065349290ca9a3e43a865990397fb9ce
ik012982i9 <a@>
2024-03-14 13:19:24 +0100

    establecer el frame

--- src/HITO1.java
+++ src/HITO1.java
@@ -1,8 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class HITO1 extends JFrame {
 
-    public static void main(String[] args) {
+    public HITO1(){
+        setPreferredSize(new Dimension(600,300));
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+
+        pack();
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new HITO1();
     }
 }
